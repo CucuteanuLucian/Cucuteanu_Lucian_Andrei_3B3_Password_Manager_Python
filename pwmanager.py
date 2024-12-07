bda40417a544f8787c54f8fc725a9ba4395ba40f
Cucuteanu Lucian-Andrei <lucian.andrei68@gmail.com>
2024-12-07 16:40:51 +0200

    Update: Error Handling, Hash idea
    Am scos clasa, toate metodele au devenit functii.
    Am adaugat error handling la restul functiilor.
    Urmeaza sa construiesc o functie hash care modifica parola si o stocheaza ca dupa sa fie verificata cu parola de la linia de comanda.

--- pwmanager.py
+++ pwmanager.py
@@ -1,28 +1,45 @@
 import sys
 import sqlite3
 
-class PM:
-    def __init__(self):
+def hash_key_write(password):
+    try:
+        file = open("hashkey.meta", "w")
+        file.write(password)
+
+    except FileNotFoundError:
+        print(f"Eroare: Fisier inexistent")
+    except Exception as e:
+        print(f"Eroare: {e}")
+
+
+def opendb():
+    try:
         print("Pornim baza de date...\n")
         global conn, cursor
         conn = sqlite3.connect("pwmanager.db")
         cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS passwords 
-        (id INTEGER PRIMARY KEY, website TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password BLOB NOT NULL,
-        salt BLOB NOT NULL )''')
+        (id INTEGER PRIMARY KEY,
+        website TEXT NOT NULL,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL)
+        ''')
         conn.commit()
+    except Exception as e:
+        print(f"Eroare: {e}")
 
-    def add(self, website, username, password):
+
+def add(website, username, password):
+    try:
         print("Execut comanda add...\n")
-        salt="abcd"
-        cursor.execute('INSERT INTO passwords (website, username, password, salt) VALUES (?, ?, ?, ?)',
-                       (website, username, password, salt))
+        cursor.execute('INSERT INTO passwords (website, username, password) VALUES (?, ?, ?)',
+                   (website, username, password))
         conn.commit()
+    except sqlite3.IntegrityError as e:
+        print("Eroare: Username existent!\n")
 
-
-    def get(self, website):
+def get(website):
+    try:
         print("Execut comanda get...\n")
         query = 'SELECT website, username, password FROM passwords where website = ?'
         cursor.execute(query, (website,))
@@ -33,29 +50,41 @@ class PM:
         else:
             print("Goala")
         print()
+    except Exception as e:
+        print(f"Eroare: {e}")
 
 
-    def remove(self, website):
+def remove(website):
+    try:
         print("Execut comanda remove...\n")
         query = 'DELETE FROM passwords where website = ?'
         cursor.execute(query, (website,))
         conn.commit()
+    except Exception as e:
+        print(f"Eroare: {e}")
 
-    def list(self):
+def list():
+    try:
         print("Execut comanda list...\n")
         cursor.execute("SELECT * from passwords")
         rows = cursor.fetchall()
         print("Lista bazei de date este:")
         if rows:
             for row in rows:
-                print(row)
+                print(f"ID: {row[0]} | Website: {row[1]} | Username: {row[2]} | Password: {row[3]}")
         else:
             print("Goala")
         print()
+    except Exception as e:
+        print(f"Eroare: {e}")
 
-    def closedb(self):
-        print("Inchidem baza de date...\n")
+
+def closedb():
+    try:
+        print("Inchidem baza de date...")
         conn.close()
+    except Exception as e:
+        print(f"Eroare: {e}")
 
 
 def main():
@@ -65,24 +94,34 @@ def main():
         print("Incearca ca input: \"pwmanager.py <master_password> -<operation> <website> <username> <password>\"")
         exit(0)
     try:
-        general = PM()
+        opendb()
+        master_password=sys.argv[1]
         operation=sys.argv[2]
         print(f"Operatia aleasa este: \"{operation}\"\n")
         if operation == '-add':
             if len(sys.argv) != 6:
                 print("Input-ul comenzii \"add\" este invalid..")
                 exit(0)
-            general.add(sys.argv[3], sys.argv[4], sys.argv[5])
+            add(sys.argv[3], sys.argv[4], sys.argv[5])
         elif operation == '-get':
-            general.get(sys.argv[3])
+            get(sys.argv[3])
         elif operation == '-remove':
-            general.remove(sys.argv[3])
+            remove(sys.argv[3])
         elif operation == '-list':
-            general.list()
+            list()
         else:
             print("Eroare: Comanda Invalida!")
             exit(0)
-        general.closedb()
+        closedb()
+
+        hash_key_write("ParolaSecreta")
+        file = open("hashkey.meta", "r")
+        file_buffer = file.read()
+        if master_password == file_buffer:
+            print("Correct Key!")
+        else:
+            print("Wrong Key!")
+            exit(0)
     except IndexError:
         print("Eroare: Incearca ca input: \"pwmanager.py <master_password> -<operation> <website> <username> <password>\"")
     except Exception as e:
